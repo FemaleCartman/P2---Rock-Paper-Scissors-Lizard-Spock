128bc11fe440eb59a2c783034622ab10a7931523
Anastacia <anastasija.teplakova@live.com>
2022-12-14 16:43:32 +0100

    Added HTML code for the rules button. Added footer code in HTML. Added comments in Javascript to help structurize it.

---
+++ rockpaperscissors/js/script.js
@@ -0,0 +1,37 @@
+// Selecting elements
+
+
+// Modal rules
+
+
+// Maximum tries = 10 , Score Limit = 3 by default
+let score = 0, tries = 0, maxTry = 10, scoreLimit = 3;
+
+
+// Show max tries and score goal below 
+
+// Game start
+
+
+// Win or lose result
+function checkedValue
+if (
+    else if
+) {
+    
+}
+
+// Player won, increase the score
+function youWin
+
+//Computer won, reduce the score
+function computerWin
+
+//Draw, score is not changing
+function draw
+
+
+function displayResult
+
+
+// Dark mode
