825adca93637ea0b4d9dd696323bc7ef63789972
Anastacia <anastasija.teplakova@live.com>
2022-12-15 14:41:36 +0100

    Added missing peace of code in CSS for darkmode. Added CSS for the game itself. Added comments and snippets of little code in JS.

--- rockpaperscissors/js/script.js
+++ rockpaperscissors/js/script.js
@@ -1,25 +1,33 @@
 // Selecting elements
-
+const document.querySelector
+const document.querySelector
+const document.querySelector
+const document.querySelector
 
 // Modal rules
-
+const openModal = function
 
 // Maximum tries = 10 , Score Limit = 3 by default
 let score = 0, tries = 0, maxTry = 10, scoreLimit = 3;
 
 
 // Show max tries and score goal below 
+"tries"
+
+"goal"
 
 // Game start
 
 
 // Win or lose result
-function checkedValue
-if (
-    else if
-) {
-    
-}
+function checkedValue(value1, value2) {
+    if (youSelection === value1 && computerOption === value2) {
+      youWin();
+    }
+    else if (youSelection === value2 && computerOption === value1) {
+      computerWin();
+    }
+  }
 
 // Player won, increase the score
 function youWin
@@ -30,8 +38,12 @@ function computerWin
 //Draw, score is not changing
 function draw
 
-
-function displayResult
+// Display result
+function displayResult ".game__start"
 
 
 // Dark mode
+
+".game__darkMode"
+
+".game__selection"
